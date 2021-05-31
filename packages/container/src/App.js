ba1140896a10e93e7f7ffaf288081738eb57a16d
Roberto Zamora <rrzu777@gmail.com>
2021-05-30 22:31:06 -0400

    ADD: fixing material ui classes conflicts

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider } from '@material-ui/core/styles'
+
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   )
 }
